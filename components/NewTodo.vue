fe24b65456dac0aaa00559cb8ae8cc262097bd08
nandita1 <nandita.kalita1@gmail.com>
2020-06-06 00:06:48 +0530

    tailwind

--- components/NewTodo.vue
+++ components/NewTodo.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="form">
-    <form>
-      <input type="text" placeholder="Type something here..." v-model.trim="todo" />
-      <button class="btn btn-primary" @click.prevent="createNew">+ Add</button>
+  <div class="my-4">
+    <form class="flex">
+      <input
+        class="flex-1 mr-4 p-1 border border-gray-400"
+        type="text"
+        placeholder="Type something here..."
+        v-model.trim="todo"
+      />
+      <button
+        class="p-1 bg-orange-500 text-white focus:outline-none"
+        @click.prevent="createNew"
+      >+ Add</button>
     </form>
-    <p v-if="showError">*Enter a todo</p>
+    <p class="mt-4 text-red-600" v-if="showError">*Enter a todo</p>
   </div>
 </template>
 
@@ -29,31 +37,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.form {
-  margin: 20px 0;
-}
-form {
-  display: flex;
-}
-p {
-  margin-top: 10px;
-  color: red;
-}
-input {
-  flex: 1;
-  margin-right: 10px;
-}
-input,
-button {
-  padding: 5px;
-  border: 1px solid #ccc;
-}
-button {
-  background-color: #fa8c16;
-  color: white;
-  border: none;
-  outline: none;
-}
-</style>
--- pages/todos/_id/index.vue
+++ pages/todos/_id/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Todo page</h1>
+  <div>
+    <h1>{{ getTodo(this.$route.params.id).name }}</h1>
+    <p>
+      Status:
+      {{
+      getTodo(this.$route.params.id).completed ? "Completed" : "Not Completed"
+      }}
+    </p>
+  </div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["getTodo"])
+  }
+};
+</script>
